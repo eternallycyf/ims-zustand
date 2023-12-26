dc980bcc553eed811adef1f0ef5c592d526be731
eternallycyf <969475322@qq.com>
2023-12-26 13:09:14 +0800

    🐛 fix: upgrade vitest alias use relative aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 import { name } from './package.json';
 
 export default defineConfig({
   esbuild: {
-    // jsxInject: "import React from 'react'",
+    jsxInject: "import React from 'react'",
   },
   test: {
     setupFiles: './tests/test-setup.ts',
     environment: 'jsdom',
     globals: true,
     alias: {
-      '@': './src',
-      [name]: './src',
+      '@': path.resolve(__dirname, './src'),
+      [name]: path.resolve(__dirname, './src'),
     },
     coverage: {
       reporter: ['text', 'text-summary', 'json', 'lcov'],
