baf0e5e25d3dc479ab87b37ae28634138141949f
eternallycyf <969475322@qq.com>
2024-05-05 12:42:47 +0800

    ✨ feat: add umd build

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  cjs: { output: 'lib' },
+  cjs: { output: 'lib', platform: 'browser' },
   esm: { output: 'es' },
+  umd: { output: 'dist' },
+  extraBabelPlugins: ['add-module-exports'],
 });
